2744990c8b74a8616cf170d7527d95e815d7746f
waleed ahmed <waleedaahmedd@gmail.com>
2021-04-25 05:06:15 +0500

    Capture and check Images

--- app/src/main/java/com/motioncamera/camera/MainActivity.java
+++ app/src/main/java/com/motioncamera/camera/MainActivity.java
@@ -1,29 +1,161 @@
 package com.motioncamera.camera;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Camera;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import java.io.File;
+
 
 public class MainActivity extends AppCompatActivity {
 
     android.hardware.Camera camera;
     FrameLayout frameLayout;
     ShowCamera showCamera;
+    static int count;
+    MyCounter timer = null;
+    byte[] imageInBytes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        frameLayout = (FrameLayout)findViewById(R.id.frame_layout);
+        frameLayout = (FrameLayout) findViewById(R.id.frame_layout);
 
         // Open the camera
 
         camera = android.hardware.Camera.open();
-        showCamera = new ShowCamera(this,camera);
+        showCamera = new ShowCamera(this, camera);
 
         frameLayout.addView(showCamera);
+
+    }
+
+    Camera.PictureCallback mPictureCallback = new Camera.PictureCallback() {
+        @RequiresApi(api = Build.VERSION_CODES.Q)
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            Log.e("called", "picture taken");
+            camera.stopPreview();
+            if (null == imageInBytes) {
+                imageInBytes = data;
+            } else {
+                Bitmap img1 = BitmapFactory.decodeByteArray(imageInBytes, 0, imageInBytes.length);
+                Bitmap img2 = BitmapFactory.decodeByteArray(data, 0, data.length);
+                int width = img1.getWidth();
+                int height = img1.getHeight();
+                int width2 = img2.getWidth();
+                int height2 = img2.getHeight();
+                long diff = 0;
+                for (int y = 0; y < height; y++) {
+                    for (int x = 0; x < width; x++) {
+                        diff += pixelDiff(img1.getColor(x, y).toArgb(), img2.getColor(x, y).toArgb());
+                    }
+                }
+                long maxDiff = 3L * 255 * width * height;
+                double percent = 100.0 * diff / maxDiff;
+                ;
+
+                // boolean diff = Arrays.equals(imageInBytes,data);
+                if (percent > 10) {
+                    try {
+                        Thread.sleep(1000);
+                        Toast.makeText(MainActivity.this, "Image Changed", Toast.LENGTH_SHORT).show();
+                        imageInBytes = data;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        }
+    };
+
+
+    public void stop(View v) {
+        timer.cancel();
+    }
+
+    private static int pixelDiff(int rgb1, int rgb2) {
+        int r1 = (rgb1 >> 16) & 0xff;
+        int g1 = (rgb1 >> 8) & 0xff;
+        int b1 = rgb1 & 0xff;
+        int r2 = (rgb2 >> 16) & 0xff;
+        int g2 = (rgb2 >> 8) & 0xff;
+        int b2 = rgb2 & 0xff;
+        return Math.abs(r1 - r2) + Math.abs(g1 - g2) + Math.abs(b1 - b2);
+    }
+
+   /* private File getOutputMediaFile() {
+        String state = Environment.getExternalStorageState();
+        if (!state.equals(Environment.MEDIA_MOUNTED)) {
+            return null;
+        } else {
+            File folder_gui = new File(Environment.getExternalStorageState() + File.separator + "GUI");
+            if (!folder_gui.exists()) {
+                folder_gui.mkdirs();
+            }
+            File outputFile = new File(folder_gui, "temp.jpg");
+            return outputFile;
+        }
+    }*/
+
+    public void captureImage(View v) {
+        if (camera != null) {
+            try {
+
+                camera.takePicture(null, null, mPictureCallback);
+
+            } catch (Exception e) {
+                showCamera = new ShowCamera(MainActivity.this, camera);
+
+                frameLayout.addView(showCamera);
+            }
+            timer = new MyCounter(1000, 1000);
+            timer.start();
+        }
+    }
+
+
+    public class MyCounter extends CountDownTimer {
+
+        public MyCounter(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onFinish() {
+            Log.e("Finish", "Timer Completed.");
+
+            timer.start();
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+
+            try {
+
+                camera.startPreview();
+                camera.takePicture(null, null, mPictureCallback);
+
+            } catch (Exception e) {
+                showCamera = new ShowCamera(MainActivity.this, camera);
+
+                frameLayout.addView(showCamera);
+            }
+
+        }
     }
 }
--- app/src/main/java/com/motioncamera/camera/ShowCamera.java
+++ app/src/main/java/com/motioncamera/camera/ShowCamera.java
@@ -4,17 +4,22 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.hardware.Camera;
+import android.text.PrecomputedText;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import androidx.annotation.NonNull;
+import androidx.core.text.PrecomputedTextCompat;
 
 import java.io.IOException;
+import java.util.List;
 
 public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
 
     Camera camera;
     SurfaceHolder holder;
+
     public ShowCamera(Context context, Camera camera) {
         super(context);
         this.camera = camera;
@@ -23,13 +28,38 @@ public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
     }
 
     @Override
-    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+    public void surfaceChanged(@NonNull SurfaceHolder mHolder, int format, int width, int height) {
+        Log.d("Function", "surfaceChanged iniciado");
+        if (mHolder.getSurface() == null) {
+            // preview surface does not exist
+            return;
+        }
 
+        // stop preview before making changes
+        try {
+            camera.stopPreview();
+        } catch (Exception e) {
+            // ignore: tried to stop a non-existent preview
+        }
+
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+
+
+        // start preview with new settings
+        try {
+            camera.setPreviewDisplay(mHolder);
+            camera.startPreview();
+
+        } catch (Exception e) {
+            Log.d("TAG", "Error starting camera preview: " + e.getMessage());
+        }
     }
 
     @Override
     public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
-
+        camera.stopPreview();
+        camera.release();
     }
 
     @Override
@@ -37,19 +67,23 @@ public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
         Camera.Parameters params = camera.getParameters();
 
         // change orientation if the camera
-        //shoaib
+        List<Camera.Size> sizes = params.getSupportedPictureSizes();
+        Camera.Size mSize = null;
 
-        if (this.getResources().getConfiguration().orientation!= Configuration.ORIENTATION_LANDSCAPE)
-        {
-            params.set("orientation","portrait");
+        for (Camera.Size size : sizes) {
+            mSize = size;
+        }
+
+        if (this.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE) {
+            params.set("orientation", "portrait");
             camera.setDisplayOrientation(90);
             params.setRotation(90);
-        }
-        else {
-            params.set("orientation","landscape");
+        } else {
+            params.set("orientation", "landscape");
             camera.setDisplayOrientation(0);
             params.setRotation(0);
         }
+        params.setPictureSize(mSize.width, mSize.height);
 
         camera.setParameters(params);
 
@@ -57,8 +91,7 @@ public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
             camera.setPreviewDisplay(holder);
 
             camera.startPreview();
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
